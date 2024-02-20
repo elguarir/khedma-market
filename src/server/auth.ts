1f60dcf5fae9815a3911d0d87cb443c208e2651b
MohamedElguarir <120427922+MohamedElguarir@users.noreply.github.com>
2024-02-20 12:43:55 +0100

    fix: eslint quick fix

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -49,27 +49,27 @@ export const authOptions: NextAuthOptions = {
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
-    Credentials({
-      async authorize(credentials) {
-        const validatedFields = LoginSchema.safeParse(credentials);
+    // Credentials({
+    //   async authorize(credentials) {
+    //     const validatedFields = LoginSchema.safeParse(credentials);
 
-        if (validatedFields.success) {
-          const { email, password } = validatedFields.data;
+    //     if (validatedFields.success) {
+    //       const { email, password } = validatedFields.data;
           
-          const user = await getUserByEmail(email);
-          if (!user || !user.password) return null;
+    //       const user = await getUserByEmail(email);
+    //       if (!user || !user.password) return null;
 
-          const passwordsMatch = await bcrypt.compare(
-            password,
-            user.password,
-          );
+    //       const passwordsMatch = await bcrypt.compare(
+    //         password,
+    //         user.password,
+    //       );
 
-          if (passwordsMatch) return user;
-        }
+    //       if (passwordsMatch) return user;
+    //     }
 
-        return null;
-      }
-    }),
+    //     return null;
+    //   }
+    // }),
   ],
   pages: {
     signIn: "/sign-in",
