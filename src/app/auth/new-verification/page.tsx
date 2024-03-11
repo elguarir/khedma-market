d2f0cbd6106e63442c24856fea4621d021f9519a
Mohamed Elguarir <mohaelguarir@gmail.com>
2024-03-11 15:23:35 +0000

    fix:  verification route fix

--- src/app/auth/new-verification/page.tsx
+++ src/app/auth/new-verification/page.tsx
@@ -1,8 +1,11 @@
 import { NewVerificationForm } from "@/components/auth/new-verification-form";
+import { Suspense } from "react";
 
 const NewVerificationPage = () => {
   return ( 
+    <Suspense fallback="Loading...">
     <NewVerificationForm />
+    </Suspense>
    );
 }
  
